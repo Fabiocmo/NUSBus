e1cf3fe4c47f054340faa3e61a30d2f87f644920
Weiming <wingalong@gmail.com>
2016-10-26 21:46:51 +0800

    Fix error with retroift

--- app/src/main/java/me/zhuangweiming/nusbus/services/BusStopApi.java
+++ app/src/main/java/me/zhuangweiming/nusbus/services/BusStopApi.java
@@ -4,7 +4,7 @@ import me.zhuangweiming.nusbus.model.BusStopResponse;
 import me.zhuangweiming.nusbus.model.ShuttleResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by Thibault on 16/10/2016.
@@ -14,7 +14,7 @@ public interface BusStopApi {
     @GET("/eventservice.svc/BusStops")
     Call<BusStopResponse> getBusStops();
 
-    @GET("/eventservice.svc/Shuttleservice?busstopname={busstop}")
-    Call<ShuttleResponse> getShuttles(@Path("busstop") String busStopName);
+    @GET("/eventservice.svc/Shuttleservice")
+    Call<ShuttleResponse> getShuttles(@Query("busstopname") String busStopName);
 
 }
