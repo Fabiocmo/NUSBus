6917375facd1d581177c2953d523e43ea7630d4a
Weiming <wingalong@gmail.com>
2016-10-29 03:21:41 +0800

    Add toolbar and menu in toolbar to main; Change tabs in Shuttle activity to tool bar;

--- app/src/main/java/me/zhuangweiming/nusbus/view/MainActivity.java
+++ app/src/main/java/me/zhuangweiming/nusbus/view/MainActivity.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import me.zhuangweiming.nusbus.R;
 import me.zhuangweiming.nusbus.model.Tab;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar mToolbar;
     private TabLayout tabLayout;
     private ViewPager tabViewPager;
 
@@ -26,12 +29,36 @@ public class MainActivity extends AppCompatActivity {
 //        busStopFragTransaction.add(R.id.bus_stop_fragment, busStopFragment);
 //        busStopFragTransaction.commit();
 
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
         tabViewPager = (ViewPager) findViewById(R.id.viewpager);
         Tab.setupMainActivityViewPager(tabViewPager, getSupportFragmentManager());
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(tabViewPager);
         Tab.setupTabIcons(tabLayout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
--- app/src/main/java/me/zhuangweiming/nusbus/view/ShuttleActivity.java
+++ app/src/main/java/me/zhuangweiming/nusbus/view/ShuttleActivity.java
@@ -1,35 +1,41 @@
 package me.zhuangweiming.nusbus.view;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import me.zhuangweiming.nusbus.R;
-import me.zhuangweiming.nusbus.model.Tab;
 
 
 public class ShuttleActivity extends AppCompatActivity {
-    private TabLayout tabLayout;
-    private ViewPager tabViewPager;
-
+    private Toolbar shuttleToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shuttle);
-//
-//        FragmentManager fragmentManager = this.getSupportFragmentManager();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        ShuttleFragment shuttleFragment = new ShuttleFragment();
-//        fragmentTransaction.add(R.id.shuttle_fragment, shuttleFragment);
-//        fragmentTransaction.commit();
 
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        ShuttleFragment shuttleFragment = new ShuttleFragment();
+        fragmentTransaction.add(R.id.shuttle_fragment, shuttleFragment);
+        fragmentTransaction.commit();
 
-        tabViewPager = (ViewPager) findViewById(R.id.viewpager);
-        Tab.setupShuttleActivityViewPager(tabViewPager, getSupportFragmentManager());
+        shuttleToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(shuttleToolbar);
 
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(tabViewPager);
-        Tab.setupTabIcons(tabLayout);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        shuttleToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
